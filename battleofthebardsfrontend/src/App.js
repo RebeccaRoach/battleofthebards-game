e2ad26bed44a5094b7634542d23c21f55011ba3d
RebeccaRoach <rebeccaroach12@gmail.com>
2020-07-09 21:03:51 -0700

    Changed Home to PoemFram component for organization

--- battleofthebardsfrontend/src/App.js
+++ battleofthebardsfrontend/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import PoemFrame from "./components/PoemFrame"
 import './App.css';
-import Home from "./components/Home.js"
 
-function App() {
+// helper function for Json.parse(json) to get array of shuffled poem objs
+// from raw json, make the poem list of objects here
+// use context with two arrays, one for unread poems, one for read poems
+
+// helper function to track num guesses per question
+
+// helper function to check if guess is correct
+// and adds score
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +19,18 @@ function App() {
           Display a poem here:
         </p>
       </header>
-      <Home />
+      <PoemFrame />
+      {/* FOOTER */}
+      <div class="mt-5 pt-5 pb-5 footer">
+        <div class="container">
+          <div class="row mt-5">
+            <div class="col copyright">
+              <p>Made by Becca</p>
+              <p><small>© 2020. All Rights Reserved.</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ battleofthebardsfrontend/src/components/PoemFrame.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Poem from './Poem'
+import Question from './Question'
+import Clue from './Clue'
+// import './App.css';
+
+// loops into App state context to conditionally render stateful things in this component (not other children)
+
+const PoemFrame = () => {
+  return (
+    <div className="App">
+      {/* <Poem /> */}
+      <Question />
+      {/* <Clue /> */}
+    </div>
+  );
+}
+
+export default PoemFrame;
