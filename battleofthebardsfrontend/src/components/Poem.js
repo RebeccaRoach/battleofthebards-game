5e9c8b90b788aae00b9c5347fe9c18a586632aee
RebeccaRoach <rebeccaroach12@gmail.com>
2020-07-10 14:08:26 -0700

    Added state for conditional rendering of question and clues

--- battleofthebardsfrontend/src/components/Poem.js
+++ battleofthebardsfrontend/src/components/Poem.js
@@ -6,15 +6,17 @@ const Poem = () => {
   const game = useContext(GameContext);
 
   return (
-    <div className="poem-card">
-      <div className="card">
-        <div className="card-header">
-          <h4>{game.currentPoem.title}</h4>
-          <p>by {game.currentPoem.author}, {game.currentPoem.year}</p>
-        </div>
-        <div className="card-body">
-          <div>
-            <pre><span className="card-text">{game.currentPoem.text}</span></pre>
+    <div className="poem-container">
+      <div className="inner-p-c">
+        <div className="card">
+          <div className="card-header">
+            <h4>{game.currentPoem.title}</h4>
+            <p>by {game.currentPoem.author}, {game.currentPoem.year}</p>
+          </div>
+          <div className="card-body">
+            <div>
+              <pre><span className="card-text">{game.currentPoem.text}</span></pre>
+            </div>
           </div>
         </div>
       </div>
--- battleofthebardsfrontend/src/components/PoemFrame.js
+++ battleofthebardsfrontend/src/components/PoemFrame.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState }from 'react';
 import Poem from './Poem'
 import Question from './Question'
 import Clue from './Clue'
-// import './App.css';
 
 // loops into App state context to conditionally render stateful things in this component (not other children)
 
-const PoemFrame = (poem) => {
+const PoemFrame = () => {
+
+  const [displayedPoemQuestion, setDisplayedPoemQuestion] = useState(false);
+  const [displayClues, setDisplayClues] = useState(0);
+
+  const getQuestion = () => {
+    setDisplayedPoemQuestion(true);
+  }
+
+  const getClue = () => {
+    if (displayClues == 0) {
+      setDisplayClues(1);
+    }
+  }
+  
+  const handleClick = () => {
+    // TODO: figure out how to advance the poem frame based on game context
+    console.log("Clicked!");
+  }
+
   return (
     <div className="poem-frame-container">
       <div className="row">
@@ -14,10 +32,14 @@ const PoemFrame = (poem) => {
           <Poem />
         </div>
         <div className="column">
-          <Question />
-          <button className="pizazz-btn">Question</button>
-          <h4>Clue Section:</h4>
-          <Clue />
+          {displayedPoemQuestion && <Question />}
+          {!displayedPoemQuestion && <button onClick={getQuestion} className="pizazz-btn">Show Question</button>}
+          {displayedPoemQuestion && <button onClick={getClue} className="pizazz-btn">Show Clue</button>}
+          {displayClues == 1 ? <Clue /> : ""}
+          <div className="next-container">
+            <input onClick={handleClick} className="advance-arrow" type="image" src="https://img.icons8.com/nolan/64/circled-chevron-right.png" />
+            {/* <input className="back-arrow" type="image" src="https://img.icons8.com/nolan/72/circled-chevron-left.png" /> */}
+          </div>
         </div>
       </div>
     </div>
