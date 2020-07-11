65f6acb15c6859bf7e9b10c58e94b39351699752
RebeccaRoach <rebeccaroach12@gmail.com>
2020-07-10 18:12:30 -0700

    Implemented checkAnswer to handle user input. Updated handleSubmit to not check empty string

--- battleofthebardsfrontend/src/components/Question.js
+++ battleofthebardsfrontend/src/components/Question.js
@@ -1,24 +1,34 @@
 import React, { useState, useContext } from 'react';
-import {GameContext} from '../App';
+import { GameContext } from '../App';
 
-const Question = () => {
+const Question = ({ question, onSuccess }) => {
 
   const game = useContext(GameContext);
   const [guess, setGuess] = useState("");
 
+  const checkAnswer = (answer) => {
+
+    if (guess === "") {
+      alert("Please enter something to make a valid guess.");
+      return;
+    } else if (answer === question.answer) {
+      // bootstrap toast message instead of alert?
+      alert("You guessed correctly!");
+      onSuccess();
+      game.setScore(game.score + question.score);
+    } else {
+      alert("Try again!");
+      // TODO: with toast message say how many clues they have left??
+    }
+  }
+
   const handleChange = (e) => {
-    e.preventDefault();
     setGuess(e.target.value);
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // to ensure users can't input empty guess
-    // TODO: make submit button grayed out instead?
-    if (guess === "") {
-      alert("Please enter something to make a valid guess.");
-      return;
-    };
+    checkAnswer(guess);
 
     // clear for next guess
     setGuess("");
@@ -27,23 +37,19 @@ const Question = () => {
   return (
     <div className="question-container">
       <div>
-        <p>{game.currentPoem.questions.map((q) => {
-          return (
-            <div>
-              <p>{q.text}</p>
-              <form onSubmit={handleSubmit}>
-                <input
-                  type="text"
-                  placeholder="Your answer"
-                  name="userAnswer"
-                  onChange={handleChange}
-                  value={guess}
-                />
-                <input value="submit" type="submit" className="btn-dark"/>
-              </form>
-            </div>
-          )
-        })}</p>
+        <div>
+          <p>{question.text}</p>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Your answer"
+              name="userAnswer"
+              onChange={handleChange}
+              value={guess}
+            />
+            <input value="submit" type="submit" className="btn-dark" />
+          </form>
+        </div>
       </div>
     </div>
   );
