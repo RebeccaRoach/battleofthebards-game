ad5ddddba76f6d90e405f6e7bec1acb078df84fb
RebeccaRoach <rebeccaroach12@gmail.com>
2020-07-21 14:11:06 -0700

    Updates

---
+++ precache-manifest.ef1860fe0e4665da97d1a8b7a1a6ff67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7b4d135d96afa9be66eb4dc580d351",
+    "url": "/battleofthebards-game/index.html"
+  },
+  {
+    "revision": "a683aa46cc0766d479a9",
+    "url": "/battleofthebards-game/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cfd250b4825c49cb2c2e",
+    "url": "/battleofthebards-game/static/css/main.5531e900.chunk.css"
+  },
+  {
+    "revision": "a683aa46cc0766d479a9",
+    "url": "/battleofthebards-game/static/js/2.6f51a01f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/battleofthebards-game/static/js/2.6f51a01f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd250b4825c49cb2c2e",
+    "url": "/battleofthebards-game/static/js/main.4cadd75b.chunk.js"
+  },
+  {
+    "revision": "94b865376249d948f372",
+    "url": "/battleofthebards-game/static/js/runtime-main.fa608055.js"
+  }
+]);
